956516441cb04adb4dc2838e71fc62d8010f91dd
Fran <francinele.coliveira@gmail.com>
2022-07-19 16:40:01 -0300

    feat: create controller for withdrawValues func

--- src/controllers/balance.controller.ts
+++ src/controllers/balance.controller.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from 'express';
-import depositService from '../services/balance.service';
+import balanceService from '../services/balance.service';
 
 const depositValues = async (req: Request, res: Response): Promise<Response> => {
   if (req.body.amount < 1) {
     return res.status(400).json({ message: 'The amount must be equal or greater than 1,00' });
   } else {
-    const deposit = await depositService.depositValues(req.body);
+    const deposit = await balanceService.depositValues(req.body);
     return res.status(201).json(deposit);
   }
 };
 
-export default { depositValues };
+const withdrawValues = async (req: Request, res: Response): Promise<Response> => {
+  const withdraw = await balanceService.withdrawValues(req.body);
+    return res.status(201).json(withdraw);
+};
+
+export default { depositValues, withdrawValues };
