bda28cd1cc398a7adcad58875b6bf1ba72705929
Fran <francinele.coliveira@gmail.com>
2022-07-18 22:17:28 -0300

    feat: initiate func to update assetAvailability

--- src/services/buyAsset.service.ts
+++ src/services/buyAsset.service.ts
@@ -7,4 +7,11 @@ const buyAsset = async (asset: IAsset) => {
   return { id: insertId, assetId, quantity, clientId };
 };
 
+// const updateAssetAvailability = async (assetId: number, quantity: number) => {
+//   //debita do total da disponibilidade
+//   const availability = await buyAssetModel.getAssetInfo(assetId);
+//   return buyAssetModel.updateAsset(Number(availability[0].quantity) - quantity, assetId);
+   
+// };
+
 export default { buyAsset };
