0e33187e3b3293a1c96722dbb1eaa34eee3e714c
Fran <francinele.coliveira@gmail.com>
2022-07-19 17:02:10 -0300

    feat[balance.controller]: add validations of the amount at withdrawValues function

--- src/controllers/balance.controller.ts
+++ src/controllers/balance.controller.ts
@@ -11,8 +11,12 @@ const depositValues = async (req: Request, res: Response): Promise<Response> => 
 };
 
 const withdrawValues = async (req: Request, res: Response): Promise<Response> => {
+  if (req.body.amount < 1) {
+    return res.status(400).json({ message: 'The amount must be equal or greater than 1,00' });
+  } else {
   const withdraw = await balanceService.withdrawValues(req.body);
     return res.status(201).json(withdraw);
+  };
 };
 
 export default { depositValues, withdrawValues };
