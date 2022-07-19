ed7f6216aa06a00017207b89d8ef585d45733c88
Fran <francinele.coliveira@gmail.com>
2022-07-19 15:55:06 -0300

    feat: validation of the amount deposited

--- src/controllers/balance.controller.ts
+++ src/controllers/balance.controller.ts
@@ -2,8 +2,12 @@ import { Request, Response } from 'express';
 import depositService from '../services/balance.service';
 
 const depositValues = async (req: Request, res: Response): Promise<Response> => {
-  const deposit = await depositService.depositValues(req.body);
-  return res.status(201).json(deposit);
+  if (req.body.amount < 1) {
+    return res.status(400).json({ message: 'The amount must be equal or greater than 1,00' });
+  } else {
+    const deposit = await depositService.depositValues(req.body);
+    return res.status(201).json(deposit);
+  }
 };
 
 export default { depositValues };
