7efa16a7419c1524cd2f49bb23b9aaa636f8822b
Fran <francinele.coliveira@gmail.com>
2022-07-19 19:56:29 -0300

    feat[buyAsset.service]: impl validations and functions to update infos at DB

--- src/services/buyAsset.service.ts
+++ src/services/buyAsset.service.ts
@@ -3,15 +3,27 @@ import { IAsset } from '../interfaces/asset.interface';
 
 const buyAsset = async (asset: IAsset) => {
   const { assetId, quantity, clientId } = asset;
-  const { insertId } = await buyAssetModel.buyAsset(asset);
-  return { id: insertId, assetId, quantity, clientId };
+  const checkedBalance = await buyAssetModel.getClientAccountInfo(clientId);
+  const clientBalance = Number(checkedBalance[0].account_balance);
+  const assetInfos = await buyAssetModel.getAssetInfo(assetId);
+  const availableQty = Number(assetInfos[0].available_qty);
+  const operationTotal = quantity * Number(assetInfos[0].unit_price);
+  const newBalance = clientBalance - operationTotal;
+  const newAmountInvested = Number(checkedBalance[0].amount_invested) + operationTotal;
+  const newAvailability = availableQty - quantity;
+  
+  if(clientBalance < operationTotal || undefined) {
+    return { message: `You don't have the necessary values for this operation in your account`}
+  };
+  if (availableQty < quantity) {
+    return { message: `The total of ${quantity} is not available`}; 
+  };
+  await buyAssetModel.uptadeClientBalance(clientId, newBalance); //clients
+  await buyAssetModel.updateAsset(assetId, newAvailability); //stocks
+  await buyAssetModel.buyAsset(asset); //transactions
+  await buyAssetModel.updateClientInvestments(clientId, newAmountInvested) //update client amount_invested
+  
+  return { message: `${operationTotal} invested in ${assetInfos[0].ticket} successfully`}
 };
-
-// const updateAssetAvailability = async (assetId: number, quantity: number) => {
-//   //debita do total da disponibilidade
-//   const availability = await buyAssetModel.getAssetInfo(assetId);
-//   return buyAssetModel.updateAsset(Number(availability[0].quantity) - quantity, assetId);
-   
-// };
 
 export default { buyAsset };
