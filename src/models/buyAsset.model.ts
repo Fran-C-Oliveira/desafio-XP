7998fbe4dec49d5dbaed1764ed83fb34fb659aa8
Fran <francinele.coliveira@gmail.com>
2022-07-19 19:54:04 -0300

    feat[buyAsset.model]: create function to update client balance

--- src/models/buyAsset.model.ts
+++ src/models/buyAsset.model.ts
@@ -34,10 +34,18 @@ const updateAsset = async (assetId: number, quantity: number): Promise<ResultSet
   return result as ResultSetHeader;
 };
 
+const uptadeClientBalance = async (clientId: number, newBalance: number): Promise<ResultSetHeader> => {
+  const [result] = await connection.execute<ResultSetHeader>(
+    `UPDATE xpStocks.clients SET account_balance = ? WHERE id = ?`,
+    [newBalance, clientId],
+  );
+  return result as ResultSetHeader;
+};
 
 export default {
   buyAsset,
   getAssetInfo,
   getClientAccountInfo,
   updateAsset,
+  uptadeClientBalance,
 };
