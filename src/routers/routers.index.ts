b6cf2b12693899ca9aec55a08daa08f90d9617e2
Fran <francinele.coliveira@gmail.com>
2022-07-19 10:35:14 -0300

    impl[routers.index]: route /assets

--- src/routers/routers.index.ts
+++ src/routers/routers.index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import investmentRoutes from './investment.routes';
+import assetRoutes from './asset.routes';
 
 const routers = Router();
 
 routers.use('/investments', investmentRoutes);
+routers.use('/assets', assetRoutes);
 
 export default routers;
