3e68774980403a076f7d3d269200f0aa586b9b7d
Fran <francinele.coliveira@gmail.com>
2022-07-19 10:56:09 -0300

    feat: implements error handling if the client has no assets

--- src/controllers/asset.controller.ts
+++ src/controllers/asset.controller.ts
@@ -5,6 +5,9 @@ const getAssetByClientId = async (req: Request, res: Response): Promise<Response
   const { id } = req.params;
   const clientId = Number(id);
   const asset = await assetService.getByClientId(clientId);
+  if (asset === undefined) {
+    return res.status(400).json({ message: 'This client does not possess any assets'});
+  }
   return res.status(201).json(asset);
 };
 
--- src/services/asset.service.ts
+++ src/services/asset.service.ts
@@ -2,11 +2,17 @@ import assetModel from '../models/asset.model';
 
 const getByClientId = async (clientId: number) => {
   const asset = await assetModel.getClientAssets(clientId);
-  return {
-    clientId: clientId,
-    ticketId: Number(asset[0].ticket_id),
-    quantity: Number(asset[0].quantity),
-    valor: Number(asset[0].unit_price),
+  try {
+    return {
+      clientId: clientId,
+      ticketId: Number(asset[0].ticket_id),
+      quantity: Number(asset[0].quantity),
+      valor: Number(asset[0].unit_price),
+    }
+  } catch(e) {
+    if (asset === undefined) {
+      throw new Error("This client does not possess any assets");
+    };
   }
 };
 
