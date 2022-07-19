ec1be6d05afdc132b80ddc825b4c4949f70eee5c
Fran <francinele.coliveira@gmail.com>
2022-07-19 15:46:35 -0300

    impl: route /account

--- src/routers/routers.index.ts
+++ src/routers/routers.index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import investmentRoutes from './investment.routes';
 import assetRoutes from './asset.routes';
+import balanceRoutes from './balance.routes';
 
 const routers = Router();
 
 routers.use('/investments', investmentRoutes);
 routers.use('/assets', assetRoutes);
+routers.use('/account', balanceRoutes);
 
 export default routers;
