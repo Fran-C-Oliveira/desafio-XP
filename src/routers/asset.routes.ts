0e8f945c77195f4f0d5d9c48d485f245e7808af3
Fran <francinele.coliveira@gmail.com>
2022-07-19 11:09:36 -0300

    impl: endpoint GET for /assets/id route

--- src/routers/asset.routes.ts
+++ src/routers/asset.routes.ts
@@ -5,5 +5,6 @@ import assetController from '../controllers/asset.controller';
 const routers = Router();
 
 routers.get('/client/:id', assetController.getAssetByClientId);
+routers.get('/:id', assetController.getAssetById);
 
 export default routers;
