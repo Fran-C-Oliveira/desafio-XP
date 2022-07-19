55fd90dcff55b9eceeee31a63fc7250b6f8c6d73
Fran <francinele.coliveira@gmail.com>
2022-07-19 19:53:17 -0300

    feat[buyAsset.model]: create function to get Account info of client

--- src/models/buyAsset.model.ts
+++ src/models/buyAsset.model.ts
@@ -19,6 +19,13 @@ const buyAsset = async (asset: IAsset): Promise<ResultSetHeader> => {
   return result as ResultSetHeader;
 };
 
+const getClientAccountInfo = async (clientId: number): Promise<RowDataPacket[]> => {
+  const [result] = await connection.execute(
+    'SELECT * FROM xpStocks.clients WHERE id = ?', [clientId],
+  );
+  return result as RowDataPacket[];
+};
+
 const updateAsset = async (assetId: number, quantity: number): Promise<ResultSetHeader> => {
   const [result] = await connection.execute<ResultSetHeader>(
     'UPDATE xpStocks.stocks SET available_qty = ? WHERE id = ?', 
@@ -27,4 +34,10 @@ const updateAsset = async (assetId: number, quantity: number): Promise<ResultSet
   return result as ResultSetHeader;
 };
 
-export default { buyAsset, getAssetInfo, updateAsset };
+
+export default {
+  buyAsset,
+  getAssetInfo,
+  getClientAccountInfo,
+  updateAsset,
+};
