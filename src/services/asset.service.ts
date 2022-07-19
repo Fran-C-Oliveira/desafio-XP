952a48abe9e57e3309e686f05852e9236cd8a28b
Fran <francinele.coliveira@gmail.com>
2022-07-19 11:07:26 -0300

    feat: create service for getAssetById

--- src/services/asset.service.ts
+++ src/services/asset.service.ts
@@ -16,4 +16,19 @@ const getByClientId = async (clientId: number) => {
   }
 };
 
-export default { getByClientId };
+const getAssetById = async (assetId: number) => {
+  const asset = await assetModel.getAssetById(assetId);
+  try {
+    return {
+      assetId: assetId,
+      quantity: Number(asset[0].available_qty),
+      valor: Number(asset[0].unit_price),
+    }
+  } catch(e) {
+    if (asset === undefined) {
+      throw new Error('This asset is not available');
+    };
+  }
+};
+
+export default { getByClientId, getAssetById };
