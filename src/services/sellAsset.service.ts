a839a2bd12a9fa543af4f53ded7e3d0d770f5fae
Fran <francinele.coliveira@gmail.com>
2022-07-19 23:28:57 -0300

    feat[sellAsset.service]: create constants to save the infos needed

--- src/services/sellAsset.service.ts
+++ src/services/sellAsset.service.ts
@@ -4,8 +4,16 @@ import { IAsset } from '../interfaces/asset.interface';
 
 const sellAsset = async (asset: IAsset) => {
   const { assetId, quantity, clientId } = asset;
-  const { insertId } = await sellAssetModel.sellAsset(asset);
-  return { id: insertId, assetId, quantity, clientId };
+  const checkedBalance = await sellAssetModel.getClientAccountInfo(clientId);
+  const clientBalance = Number(checkedBalance[0].account_balance);
+  const assetInfos = await sellAssetModel.getAssetInfo(assetId);
+  const availableQty = Number(assetInfos[0].available_qty);
+  const operationTotal = quantity * Number(assetInfos[0].unit_price);
+  const newBalance = clientBalance + operationTotal;
+  const newAmountInvested = Number(checkedBalance[0].amount_invested) - operationTotal;
+  const newAvailability = availableQty + quantity;
+  const clientAssetsInfo = await sellAssetModel.getClientAssets(assetId);
+
 };
 
 export default { sellAsset };
