47766767034cbb9221cadeef32b653a54a16c47a
Fran <francinele.coliveira@gmail.com>
2022-07-21 23:31:34 -0300

    feat: initiate tests for route assets/assetId

--- src/tests/listAssetsById.test.ts
+++ src/tests/listAssetsById.test.ts
@@ -3,7 +3,7 @@ import app from '../app';
 import assetService from '../services/asset.service';
 
 jest.mock('../services/asset.service');
-describe("1 - Validations for endpoint GET at assets route", () => {
+describe('1 - Validations for endpoint GET at assets/clients route', () => {
   beforeEach(() => {
     assetService.getAssetsByClientId = jest.fn();
   });
@@ -13,11 +13,11 @@ describe("1 - Validations for endpoint GET at assets route", () => {
   
   it('Validate if returns error if asset is not available', async () => {
     (<jest.Mock>assetService.getAssetsByClientId).mockReturnValue(undefined);
-    const result = await request(app).get('/assets/3');
+    const result = await request(app).get('/assets/client/3');
     expect(result.statusCode).toEqual(400);
-    expect(result.body).toEqual({"message":"This asset is not available"})
+    expect(result.body).toEqual({ "message": 'This client does not possess any assets' });
   });
-  it('Validate if it is possible to find a asset by its id sucessfully', async () => {
+  it('Validate if it is possible to find a asset by clientId sucessfully', async () => {
     const assets = [
       {
         ticketId: 1,
@@ -34,9 +34,17 @@ describe("1 - Validations for endpoint GET at assets route", () => {
     ];
     (<jest.Mock>assetService.getAssetsByClientId).mockReturnValue(assets);
     const result = await request(app).get('/assets/client/3');
-    console.log(result.body);
     
     expect(result.statusCode).toEqual(201);
     expect(result.body).toEqual(assets);
+  });
+});
+
+describe('2 - Validations for endpoint GET at assets/assetId route', () => {
+  beforeEach(() => {
+    assetService.getAssetById = jest.fn();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 });
