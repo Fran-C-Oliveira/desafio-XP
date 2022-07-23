edad21a6e57e8e28a92497ac597a8990df202d6a
Fran <francinele.coliveira@gmail.com>
2022-07-23 16:55:34 -0300

    feat[balance.controller]: adding try&catch method to get "dinamic" statusCode

--- src/controllers/balance.controller.ts
+++ src/controllers/balance.controller.ts
@@ -13,19 +13,26 @@ const depositValues = async (req: Request, res: Response): Promise<Response> => 
 const withdrawValues = async (req: Request, res: Response): Promise<Response> => {
   if (req.body.amount < 1) {
     return res.status(400).json({ message: 'The amount must be equal or greater than 1,00' });
-  } else {
-  const withdraw = await balanceService.withdrawValues(req.body);
-    return res.status(201).json(withdraw);
+  } 
+  try {
+   const withdraw = await balanceService.withdrawValues(req.body);
+     return res.status(201).json(withdraw);
+  } catch(err){
+    return res.status(400).json({ 
+      message: `You don't have the necessary values for this operation in your account` }
+    );
   };
 };
 
 const getAccountInfo = async (req: Request, res: Response): Promise<Response> => {
   const id = Number(req.params.id);
-  const account = await balanceService.getAccountInfo(id);
-  if(!account) {
-    return res.status(400).json({ message: 'Invalid account number' });
-  }
+
+  try {
+    const account = await balanceService.getAccountInfo(id);
     return res.status(201).json(account);
+  } catch(e) {
+    return res.status(400).json({ message: 'Invalid account number' });
+  };
 };
 
 export default { depositValues, withdrawValues, getAccountInfo };
