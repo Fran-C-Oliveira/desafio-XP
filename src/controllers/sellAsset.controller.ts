5610e41ee71be10deb0cb67d72d7b23e4cb8b6a5
Fran <francinele.coliveira@gmail.com>
2022-07-23 19:58:49 -0300

    feat[sellAsset.controller]: add try&catch function to receive the correct error message;         also add function to handle with problem at error type.

--- src/controllers/sellAsset.controller.ts
+++ src/controllers/sellAsset.controller.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from 'express';
 import sellAssetService from '../services/sellAsset.service';
+import { getErrorMessage, reportError } from '../utils/errorMessage';
 
 const sellAsset = async (req: Request, res: Response): Promise<Response> => {
-  const asset = await sellAssetService.sellAsset(req.body);
-  return res.status(201).json(asset);
+  try {
+    const asset = await sellAssetService.sellAsset(req.body);
+    return res.status(201).json(asset);
+  } catch(error) {
+    return reportError(res, { message: getErrorMessage(error) });
+  };
 };
 
 export default { sellAsset };
