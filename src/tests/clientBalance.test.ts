9b26c1741e18c17abfda00ec073af0b79c39066b
Fran <francinele.coliveira@gmail.com>
2022-07-23 00:47:47 -0300

    feat[clientBalance.test]: create test to check if It is possible to make a withdrawal from the account

--- src/tests/clientBalance.test.ts
+++ src/tests/clientBalance.test.ts
@@ -47,3 +47,35 @@ describe('It is possible to deposit into the account', () => {
     expect(afterBalanceInfo).toEqual(2000);
   });
 });
+
+describe('It is possible to withdraw from the account', () => {
+  const previewBalanceInfo = [{
+    id: 1,
+    clientName: "client01",
+    accountBalance: 1500,
+    amountInvested: 0
+  }];
+
+  beforeEach(() => {
+    balanceModel.getAccountInfo = jest.fn();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('It is possible to make a withdrawal from the account', async () => {
+    const withdraw = {
+      clientId: 1,
+      amount: 1000
+    };
+    const afterBalanceInfo = previewBalanceInfo[0].accountBalance - withdraw.amount;
+    (<jest.Mock>balanceModel.getAccountInfo).mockReturnValue(previewBalanceInfo);
+    const response = await request(app).post('/account/withdraw')
+      .set('Content-type', 'application/json')
+      .send(withdraw);
+  
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toEqual({ message: `${withdraw.amount} withdrawal successfully completed` });
+    expect(afterBalanceInfo).toEqual(500);
+  });
+});
