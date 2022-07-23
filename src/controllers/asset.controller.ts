eaf8884ba88e2f191a575a3b9f1b510de02c2f07
Fran <francinele.coliveira@gmail.com>
2022-07-23 15:43:25 -0300

    docs[asset.controller] renaming function to getAssetsByClientId

--- src/controllers/asset.controller.ts
+++ src/controllers/asset.controller.ts
@@ -4,7 +4,7 @@ import assetService from '../services/asset.service';
 const getAssetByClientId = async (req: Request, res: Response): Promise<Response> => {
   const { id } = req.params;
   const clientId = Number(id);
-  const asset = await assetService.getByClientId(clientId);
+  const asset = await assetService.getAssetsByClientId(clientId);
   if (asset === undefined) {
     return res.status(400).json({ message: 'This client does not possess any assets'});
   }
