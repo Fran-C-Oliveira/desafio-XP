3fd0cbde6803aa905d36f26497fa1742cdc3ec42
Fran <francinele.coliveira@gmail.com>
2022-07-23 00:58:41 -0300

    refac[balance.model]: transforming depositValues and withdrawValues into one function for dry the code; also put alias at the return of function getAccount info for better reading and test compability

--- src/models/balance.model.ts
+++ src/models/balance.model.ts
@@ -2,27 +2,24 @@ import { ResultSetHeader, RowDataPacket } from 'mysql2';
 import connection from '../database/connection';
 import { IBalance } from '../interfaces/balance.interface';
 
-const depositValues = async (depositInfo: IBalance): Promise<ResultSetHeader> => {
+const registerBalanceHistory = async (depositInfo: IBalance, balanceType: string): Promise<ResultSetHeader> => {
   const [result] = await connection.execute<ResultSetHeader>(
     `INSERT INTO xpStocks.balance_history(client_id, amount, balance_type) 
       VALUES (?, ?, ?)`,
-    [depositInfo.clientId, depositInfo.amount, "deposit"],
-  );
-  return result as ResultSetHeader;
-};
-
-const withdrawValues = async (depositInfo: IBalance): Promise<ResultSetHeader> => {
-  const [result] = await connection.execute<ResultSetHeader>(
-    `INSERT INTO xpStocks.balance_history(client_id, amount, balance_type) 
-      VALUES (?, ?, ?)`,
-    [depositInfo.clientId, depositInfo.amount, "withdraw"],
+    [depositInfo.clientId, depositInfo.amount, balanceType],
   );
   return result as ResultSetHeader;
 };
 
 const getAccountInfo = async (clientId: number): Promise<RowDataPacket[]> => {
   const [result] = await connection.execute(
-    'SELECT * FROM xpStocks.clients WHERE id = ?', [clientId],
+    `SELECT 
+      id,
+      client_name as clientName,
+      account_balance as accountBalance,
+      amount_invested as amountInvested
+    FROM xpStocks.clients 
+    WHERE id = ?`, [clientId],
   );
   return result as RowDataPacket[];
 };
@@ -35,4 +32,4 @@ const uptadeBalance = async (clientId: number, newBalance: number): Promise<Resu
   return result as ResultSetHeader;
 };
 
-export default { depositValues, uptadeBalance, getAccountInfo, withdrawValues };
+export default { registerBalanceHistory, uptadeBalance, getAccountInfo };
