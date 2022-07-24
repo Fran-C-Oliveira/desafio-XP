40d7ac676c360879b2c8bf8c130fd5d1650650e1
Fran <francinele.coliveira@gmail.com>
2022-07-24 14:45:32 -0300

    feat[user.controller]: set try&catch for create new user 'client exists' possible error

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from 'express';
 import userService from '../services/user.service';
+import { getErrorMessage, reportError } from '../utils/errorMessage';
 
-const createUserToken = async (req: Request, res: Response): Promise<Response> => {
-  const token = await userService.newUser(req.body);
-  return res.status(201).json({ token });
+const createNewUser = async (req: Request, res: Response): Promise<Response> => {
+  try { 
+    const token = await userService.createNewUser(req.body);
+    return res.status(201).json({ token });
+  } catch (error) {
+    return reportError(res, { message: getErrorMessage(error) });
+  };
 };
 
-export default {
-  createUserToken,
-};
+export default { createNewUser };
