e630441b4ccc993f71505a8b3bbb7fe413b11b5a
Fran <francinele.coliveira@gmail.com>
2022-07-24 14:44:09 -0300

    feat[user.service]: adds verfication for users that exists

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,10 +2,13 @@ import userModel from '../models/user.model';
 import IUser from '../interfaces/user.interface';
 import jwtAuth from '../auth/jwt.auth'
 
-
-const newUser = async (user: IUser) => {
+const createNewUser = async (user: IUser) => {
+  const userExists = await userModel.checkUserByEmail(user.email);
+  if (userExists.length !== 0) {
+    throw new Error('User already registered');
+  }
   const { insertId } = await userModel.createNewUser(user);
   return jwtAuth.generateJWTToken({ id: insertId, ...user });
 };
 
-export default { newUser };
+export default { createNewUser };
