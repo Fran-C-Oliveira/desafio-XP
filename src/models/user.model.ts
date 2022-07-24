53f44dce637769ce8c0ad87d8108effb3ca400e5
Fran <francinele.coliveira@gmail.com>
2022-07-24 14:49:31 -0300

    feat[user.model]: create function to check user by email on DB

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,4 @@
-import { ResultSetHeader } from 'mysql2';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
 import IUser from '../interfaces/user.interface';
 import connection from '../database/connection';
 
@@ -11,4 +11,17 @@ const createNewUser = async (user: IUser): Promise<ResultSetHeader> => {
   return result as ResultSetHeader;
 };
 
-export default { createNewUser };
+const checkUserByEmail = async (userEmail: string): Promise<RowDataPacket[]> => {
+  const [result] = await connection.execute(
+    `SELECT 
+      id,
+      client_name as name,
+      email,
+      password
+    FROM xpStocks.clients 
+    WHERE email = ?`, [userEmail],
+  );
+  return result as RowDataPacket[];
+};
+
+export default { createNewUser, checkUserByEmail };
