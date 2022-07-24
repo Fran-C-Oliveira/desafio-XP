f1b5c87e8308090c06782f240d31933a391472ff
Fran <francinele.coliveira@gmail.com>
2022-07-24 14:57:58 -0300

    feat[user.test]: initiate the tests for the user routes

---
+++ src/tests/user.test.ts
@@ -0,0 +1,28 @@
+import request from 'supertest';
+import app from '../app';
+import userModel from '../models/user.model';
+
+jest.mock('../models/user.model');
+
+describe('1 - Test register new user', () => {
+
+  it('Test if is not possible to create a user that already exists', async () => {});
+
+  it('Test if is possible to create a new user', async () => {});
+
+
+});
+
+
+
+
+
+
+
+
+
+
+describe('2 - Test login', () => {
+
+  it('', () => {});
+});
